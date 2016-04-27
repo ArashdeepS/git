d18c937a2ab9eb803f59821956df125990e5439e
ArashdeepS <cyberoffice400@gmail.com>
2016-04-27 15:18:28 +0530

    bootstrap

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
+        <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-fUMURLTdEcpeYHly3PAwggI3l2UvdHNg/I+8LRph7hLDcAZm77YfDx3Tjum9d3vK" crossorigin="anonymous">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
+    <aline center>
+        <div class="container">
+            <div class="jumbotron">
+       <button class="btn btn-default">arash</button>
+            </div>
+        </div>
+    </aline>
+            <?php
+       
         ?>
     </body>
 </html>
