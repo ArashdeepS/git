7a2c1e5f799e5e8515d862741a014e638dc3aa4c
ArashdeepS <cyberoffice400@gmail.com>
2016-04-27 16:27:27 +0530

    html

--- index.php
+++ index.php
@@ -3,18 +3,29 @@
 <html>
     <head>
         <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-fUMURLTdEcpeYHly3PAwggI3l2UvdHNg/I+8LRph7hLDcAZm77YfDx3Tjum9d3vK" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="Main.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-    <aline center>
-        <div class="container">
-            <div class="jumbotron">
-       <button class="btn btn-default">arash</button>
-            </div>
+       <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">ArashdepS</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#home">Home</a></li>
+      <li><a href="#page1">Page 1</a></li>
+      <li><a href="#">Page 2</a></li> 
+      <li><a href="#">Page 3</a></li> 
+    </ul>
+  </div>
+</nav>
+    <backgroung-image src="image/bg.jpg"></backgroung-image>
+        <div id="#page1" class="banner">  
+            <div align="center" class="font"><h1>ArashdeepS</h1></div>
         </div>
-    </aline>
-            <?php
+    <?php
        
         ?>
     </body>
