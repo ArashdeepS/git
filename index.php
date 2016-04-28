75faa30b4bbccd92cc1cce33163e1aa196ce7e65
ArashdeepS <cyberoffice400@gmail.com>
2016-04-28 17:26:28 +0530

    img

--- index.php
+++ index.php
@@ -2,31 +2,36 @@
 
 <html>
     <head>
-        <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-fUMURLTdEcpeYHly3PAwggI3l2UvdHNg/I+8LRph7hLDcAZm77YfDx3Tjum9d3vK" crossorigin="anonymous">
+   <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cosmo/bootstrap.min.css" rel="stylesheet" integrity="sha384-OiWEn8WwtH+084y4yW2YhhH6z/qTSecHZuk/eiWtnvLtU+Z8lpDsmhOKkex6YARr" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="Main.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-       <nav class="navbar navbar-default">
+       <nav class="navbar navbar-default ">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">ArashdepS</a>
+      <a class="navbar-brand" href="#home">ArashdepS</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#home">Home</a></li>
       <li><a href="#page1">Page 1</a></li>
-      <li><a href="#">Page 2</a></li> 
+      <li><a href="#page2">Page 2</a></li> 
       <li><a href="#">Page 3</a></li> 
     </ul>
   </div>
 </nav>
-    <backgroung-image src="image/bg.jpg"></backgroung-image>
-        <div id="#page1" class="banner">  
-            <div align="center" class="font"><h1>ArashdeepS</h1></div>
+   
+        <div id="#home">
+            <div class="banner">
+            <div class="text-center">
+            <div align="center"><h1 font-size="70">Cyber Office</h1></div>
+            <div align="center"><h3>Brings You Easy Way Of How To</h3></div>
+            </div>
+            </div>
         </div>
-    <?php
        
-        ?>
+          
+            
     </body>
 </html>
